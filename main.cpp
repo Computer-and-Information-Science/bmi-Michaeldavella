33cd7f4197d80b7b71bf1cbbe5b5f28db3ce9457
Michael D'Avella <michaeldavella2006@gmail.com>
2025-09-03 15:32:33 +0000

    bmi coversion code committed

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
+    const int height=61;
+    const int weight=130;
+
+    //convert to kilograms
+    //text
+    double kg = weight * 0.453592;
+    double meters = height * 0.0254;
+    //compute bmi
+    double bmi = kg / (meters*meters);
+
+   cout << "The BMI for this person is:"<< bmi << "." << endl;
  
 
 
